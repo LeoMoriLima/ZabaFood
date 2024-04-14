852857e3d8f8015b8678e3dd037de0c4ae02f108
Leonardo <leomoriva@gmail.com>
2024-04-14 19:05:43 -0300

    fix: added header and footer

--- public/pages/terms.js
+++ public/pages/terms.js
@@ -1,9 +1,13 @@
+import FooterMain from "../components/FooterMain.js";
+import HeaderMain from "../components/HeaderMain.js";
 import TermsPage from "../components/TermsPage.js";
 
 export default () => {
     const page = document.createElement("div");
 
+    page.appendChild(HeaderMain());
     page.appendChild(TermsPage());
+    page.appendChild(FooterMain());
 
     return page;
 }
