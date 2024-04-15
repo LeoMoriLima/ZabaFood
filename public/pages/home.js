d8fd3b550822eb4e7d593bb680c846fdef554617
Leonardo <leomoriva@gmail.com>
2024-04-15 15:31:48 -0300

    feat: Added login verify

--- public/pages/home.js
+++ public/pages/home.js
@@ -7,7 +7,7 @@ export default async () => {
     const page = document.createElement("div");
     const productId = "4c3e47b0-dce2-48c8-9b79-21709abd12bb"; // Produto: Cúrcuma com Pimenta
 
-    page.appendChild(HeaderMain());
+    page.appendChild(await HeaderMain());
     page.appendChild(mainBanner());
     // page.appendChild(await ProductBanner(productId));
     page.appendChild(FooterMain());
