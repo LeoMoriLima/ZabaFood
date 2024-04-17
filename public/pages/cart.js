c36b24c0ac1ca4f8fd06014307b1b3eb900c3059
Renato Barbosa <tenat.renato@gmail.com>
2024-04-17 17:04:47 -0300

    change: changed CartModal to show quantity and added header and footer to cart page

--- public/pages/cart.js
+++ public/pages/cart.js
@@ -1,9 +1,10 @@
 import CartModal from "../components/CartModal.js";
+import FooterMain from "../components/FooterMain.js";
+import HeaderMain from "../components/HeaderMain.js";
 
 export default async () => {
     const page = document.createElement("div");
-
-    page.appendChild(await CartModal());
-
+    page.appendChild(await HeaderMain());
+    page.appendChild(FooterMain());
     return page;
 }
