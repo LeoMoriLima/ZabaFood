696cbcd4774b35cf02786149f1622678f308755b
Renato Barbosa <tenat.renato@gmail.com>
2024-04-19 12:18:42 -0300

    change: Changed user orders page style and interactions

--- public/components/userOrdersContent.js
+++ public/components/userOrdersContent.js
@@ -18,7 +18,7 @@ export default async () => {
                 "Content-Type": "application/json"
             }
         });
-    
+
         const cartData = await cartResponse.json();
 
         cartData.forEach(async (cart) => {
@@ -27,20 +27,26 @@ export default async () => {
             div.classList.add("order-div");
             orderPageDiv.appendChild(div);
 
+            const orderInfoDiv = document.createElement("div");
+            orderInfoDiv.classList.add("order-info-div")
+            div.appendChild(orderInfoDiv)
+
             const divLeft = document.createElement("div");
             divLeft.classList.add("order-div-left");
-            div.appendChild(divLeft);
+            orderInfoDiv.appendChild(divLeft);
 
             const divId = document.createElement("div");
             divId.classList.add("div-order-id");
             divLeft.appendChild(divId)
 
             const orderLabel = document.createElement("p");
+            orderLabel.classList.add("order-id-label")
             orderLabel.innerText = "ID do pedido";
             divId.appendChild(orderLabel)
 
             const orderNumber = document.createElement("p");
             const orderID = cart.id.substring(0, 4).toUpperCase();
+            orderNumber.classList.add("order-id-number")
             orderNumber.innerText = "#" + orderID
             divId.appendChild(orderNumber);
 
@@ -57,21 +63,13 @@ export default async () => {
             const formatedDate = orderDate.toLocaleDateString('pt-BR');
 
             const orderCreatedAt = document.createElement("p");
+            orderCreatedAt.classList.add("order-date-created-at")
             orderCreatedAt.innerText = formatedDate;
             divDate.appendChild(orderCreatedAt);
 
-            const divDropArrow = document.createElement("div");
-            divDropArrow.classList.add("div-drop-arrow");
-            div.appendChild(divDropArrow);
-
-            const dropArrow = document.createElement("img");
-            dropArrow.classList.add("drop-arrow-img")
-            dropArrow.src = "../assets/images/arrow-down-green.svg";
-            divDropArrow.appendChild(dropArrow)
-
             const divInfo = document.createElement("div");
             divInfo.classList.add("div-info-order");
-            div.appendChild(divInfo);
+            orderInfoDiv.appendChild(divInfo);
 
             const infoOrderPriceLabel = document.createElement("p");
             infoOrderPriceLabel.classList.add("info-order-price-label");
@@ -87,18 +85,18 @@ export default async () => {
             infoOrderLabelStatus.classList.add("info-order-label-status");
             infoOrderLabelStatus.innerText = "Status"
             divInfo.appendChild(infoOrderLabelStatus);
-        
+
             const infoOrderStatus = document.createElement("p");
             infoOrderStatus.classList.add("info-order-status");
             divInfo.appendChild(infoOrderStatus);
 
-            if(cart.status === "pending"){
+            if (cart.status === "pending") {
                 infoOrderStatus.innerText = "Pendente";
-            } else if (cart.status === "approved"){
+            } else if (cart.status === "approved") {
                 infoOrderStatus.innerText = "Aprovador";
-            } else if (cart.status === "sended"){
+            } else if (cart.status === "sended") {
                 infoOrderStatus.innerText = "Enviado";
-            } else if (cart.status === "delivered"){
+            } else if (cart.status === "delivered") {
                 infoOrderStatus.innerText = "Entregue";
             } else {
                 infoOrderStatus.innerText = "Status desconhecido"
@@ -106,7 +104,7 @@ export default async () => {
 
             const actionDiv = document.createElement("div");
             actionDiv.classList.add("action-order-div");
-            div.appendChild(actionDiv);
+            orderInfoDiv.appendChild(actionDiv);
 
             const actionTitle = document.createElement("p");
             actionTitle.classList.add("action-title")
@@ -123,68 +121,95 @@ export default async () => {
             arrowDown.src = "../assets/images/arrow-down-green.svg";
             trackShipping.appendChild(arrowDown)
 
-            trackShipping.addEventListener("mouseenter", function () {
-                const divTrack = document.createElement("div");
-                divTrack.classList.add("div-track");
-                trackShipping.appendChild(divTrack);
+            const divTrack = document.createElement("div");
+            divTrack.classList.add("div-track");
+            divTrack.style.display = "none";
+            trackShipping.appendChild(divTrack);
 
-                const trackStatus = document.createElement("h1");
-                trackStatus.classList.add("track-status");
-                trackStatus.innerText = "Status do envio";
-                divTrack.appendChild(trackStatus)
+            const trackStatus = document.createElement("h1");
+            trackStatus.classList.add("track-status");
+            trackStatus.innerText = "Status do envio";
+            divTrack.appendChild(trackStatus)
 
-                const statusCircleDiv = document.createElement("div");
-                statusCircleDiv.classList.add("status-circle-div");
-                divTrack.appendChild(statusCircleDiv);
+            const statusCircleDiv = document.createElement("div");
+            statusCircleDiv.classList.add("status-circle-div");
+            divTrack.appendChild(statusCircleDiv);
 
-                const divCircleProcessing = document.createElement("div");
-                divCircleProcessing.classList.add("div-circle-processing");
-                statusCircleDiv.appendChild(divCircleProcessing);
+            const divCircleProcessing = document.createElement("div");
+            divCircleProcessing.classList.add("div-circle-processing");
+            statusCircleDiv.appendChild(divCircleProcessing);
 
-                const imgCircleProcessing = document.createElement("img");
-                imgCircleProcessing.classList.add("img-circle-processing");
-                imgCircleProcessing.src = "../assets/images/circle-inactive-processing.svg";
-                divCircleProcessing.appendChild(imgCircleProcessing);
+            const imgCircleProcessing = document.createElement("img");
+            imgCircleProcessing.classList.add("img-circle-processing");
+            imgCircleProcessing.src = "../assets/images/circle-inactive-processing.svg";
+            divCircleProcessing.appendChild(imgCircleProcessing);
 
-                const divCircleSended = document.createElement("div");
-                divCircleSended.classList.add("div-circle-sended");
-                statusCircleDiv.appendChild(divCircleSended);
+            const divCircleSended = document.createElement("div");
+            divCircleSended.classList.add("div-circle-sended");
+            statusCircleDiv.appendChild(divCircleSended);
 
-                const imgCircleSended = document.createElement("img");
-                imgCircleSended.classList.add("img-circle-sended");
-                imgCircleSended.src = "../assets/images/circle-inactive-sended.svg";
-                divCircleSended.appendChild(imgCircleSended);
+            const imgCircleSended = document.createElement("img");
+            imgCircleSended.classList.add("img-circle-sended");
+            imgCircleSended.src = "../assets/images/circle-inactive-sended.svg";
+            divCircleSended.appendChild(imgCircleSended);
 
-                const divCircleDelivered = document.createElement("div");
-                divCircleDelivered.classList.add("div-circle-delivered");
-                statusCircleDiv.appendChild(divCircleDelivered);
+            const divCircleDelivered = document.createElement("div");
+            divCircleDelivered.classList.add("div-circle-delivered");
+            statusCircleDiv.appendChild(divCircleDelivered);
 
-                const imgCircleDelivered = document.createElement("img");
-                imgCircleDelivered.classList.add("img-circle-delivered");
-                imgCircleDelivered.src = "../assets/images/circle-inactive-delivered.svg";
-                divCircleDelivered.appendChild(imgCircleDelivered);
+            const imgCircleDelivered = document.createElement("img");
+            imgCircleDelivered.classList.add("img-circle-delivered");
+            imgCircleDelivered.src = "../assets/images/circle-inactive-delivered.svg";
+            divCircleDelivered.appendChild(imgCircleDelivered);
 
-                const statusText = document.createElement("p");
-                statusText.classList.add("status-text-track");
-                divTrack.appendChild(statusText);
+            const statusText = document.createElement("p");
+            statusText.classList.add("status-text-track");
+            divTrack.appendChild(statusText);
 
+            if (cart.status === "pending") {
+                imgCircleProcessing.src = "../assets/images/circle-active-processing.svg";
+                statusText.innerText = "Em processamento";
+            } else if (cart.status === "sended") {
+                imgCircleSended.src = "../assets/images/circle-active-sended.svg";
+                statusText.innerText = "Enviado";
+            } else if (cart.status === "delivered") {
+                imgCircleDelivered.src = "../assets/images/circle-active-delivered.svg";
+                statusText.innerText = "Entregue"
+            }
 
-                if(cart.status === "pending"){
-                    imgCircleProcessing.src = "../assets/images/circle-active-processing.svg";
-                    statusText.innerText = "Em processamento";
-                } else if (cart.status === "sended"){
-                    imgCircleSended.src = "../assets/images/circle-active-sended.svg";
-                    statusText.innerText = "Enviado";
-                } else if (cart.status === "delivered"){
-                    imgCircleDelivered.src = "../assets/images/circle-active-delivered.svg";
-                    statusText.innerText = "Entregue"
+            // trackShipping.addEventListener("mouseleave", function () {
+            //     divTrack.remove();
+            // })
+
+            let isTrackShippingOpen = false
+
+            trackShipping.addEventListener("click", function () {
+
+                if (isTrackShippingOpen) {
+                    divTrack.style.display = "none";
+                    arrowDown.classList.remove('rotate');
+                } else {
+                    divTrack.style.display = "flex";
+                    arrowDown.classList.add('rotate');
+                    divTrack.classList.toggle('show');
                 }
 
-                trackShipping.addEventListener("mouseleave", function(){
-                    divTrack.remove();
-                })
+                isTrackShippingOpen = !isTrackShippingOpen
             })
 
+            const divDropArrow = document.createElement("div");
+            divDropArrow.classList.add("div-drop-arrow");
+            actionDiv.appendChild(divDropArrow);
+
+            const dropArrowText = document.createElement("p")
+            dropArrowText.classList.add("drop-arrow-text")
+            dropArrowText.innerText = "Exibir produtos"
+            divDropArrow.appendChild(dropArrowText)
+
+            const dropArrow = document.createElement("img");
+            dropArrow.classList.add("drop-arrow-img")
+            dropArrow.src = "../assets/images/arrow-down-green.svg";
+            divDropArrow.appendChild(dropArrow)
 
             const productCartResponse = await fetch(`/api/cart_product/order/${cart.id}`);
             const productCartData = await productCartResponse.json();
@@ -192,52 +217,51 @@ export default async () => {
             const divProducts = document.createElement("div");
             divProducts.classList.add("div-order-products");
             divProducts.style.display = "none";
-            div.appendChild(divProducts);
+            dropArrowText.appendChild(divProducts);
 
             let isDropdownOpen = false; // Variável para controlar o estado do dropdown
 
-            dropArrow.addEventListener("click", async () => {
+            divDropArrow.addEventListener("click", async () => {
                 if (isDropdownOpen) {
                     divProducts.style.display = "none";
+                    divProducts.innerHTML = ""
                     dropArrow.classList.remove('rotate');
                 } else {
                     divProducts.style.display = "flex";
                     dropArrow.classList.add('rotate');
 
-                divProducts.classList.toggle('show');
+                    divProducts.classList.toggle('show');
 
-                productCartData.forEach(async (productCartItem) => {
-                    const productResponse = await fetch(`/api/product/${productCartItem.product_id}`);
-                    const productData = await productResponse.json();
+                    productCartData.forEach(async (productCartItem) => {
+                        const productResponse = await fetch(`/api/product/${productCartItem.product_id}`);
+                        const productData = await productResponse.json();
 
-                    console.log(productData);
+                        const imgDiv = document.createElement("div");
+                        imgDiv.classList.add("order-img-div");
+                        divProducts.appendChild(imgDiv);
 
-                    const imgDiv = document.createElement("div");
-                    imgDiv.classList.add("order-img-div");
-                    divProducts.appendChild(imgDiv);
+                        const circleImgDiv = document.createElement("div");
+                        circleImgDiv.classList.add("circle-img-div");
+                        imgDiv.appendChild(circleImgDiv);
 
-                    const circleImgDiv = document.createElement("div");
-                    circleImgDiv.classList.add("circle-img-div");
-                    imgDiv.appendChild(circleImgDiv);
+                        const img = document.createElement("img");
+                        img.src = productData.url_img;
+                        img.classList.add("order-img");
+                        circleImgDiv.appendChild(img);
 
-                    const img = document.createElement("img");
-                    img.src = productData.url_img;
-                    img.classList.add("order-img");
-                    circleImgDiv.appendChild(img);
+                        const productInfoDiv = document.createElement("div");
+                        productInfoDiv.classList.add("order-product-name-div");
+                        imgDiv.appendChild(productInfoDiv);
 
-                    const productInfoDiv = document.createElement("div");
-                    productInfoDiv.classList.add("order-product-name-div");
-                    imgDiv.appendChild(productInfoDiv);
+                        const productName = document.createElement("p");
+                        productName.classList.add("order-product-name");
+                        productName.innerText = productData.name
+                        productInfoDiv.appendChild(productName);
 
-                    const productName = document.createElement("p");
-                    productName.classList.add("order-product-name");
-                    productName.innerText = productData.name
-                    productInfoDiv.appendChild(productName);
-
-                    const productValue = document.createElement("p");
-                    productValue.classList.add("order-product-value");
-                    productValue.innerText = "R$" + productData.value
-                    productInfoDiv.appendChild(productValue);
+                        const productValue = document.createElement("p");
+                        productValue.classList.add("order-product-value");
+                        productValue.innerText = "R$" + productData.value
+                        productInfoDiv.appendChild(productValue);
 
                     });
                 }
@@ -250,7 +274,7 @@ export default async () => {
     } catch (error) {
         console.error(error);
     }
-    
+
 
     return orderPageDiv;
 }
