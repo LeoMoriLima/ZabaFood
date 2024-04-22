40cad40772ca28e9b702792a8d9547fc8898e1b7
Renato Barbosa <tenat.renato@gmail.com>
2024-04-22 14:20:58 -0300

    feat and fix: improved loading speed of product and products pages and added skeletons for product-related components

---
+++ public/components/ProductCardRecommendedSkeleton.js
@@ -0,0 +1,13 @@
+import LoadingComponent from "./LoadingComponent.js"
+
+export default (id) => {
+    const skeletonDiv = document.createElement("div");
+
+    skeletonDiv.id = id
+
+    skeletonDiv.classList.add("product-card-recommended");
+
+    skeletonDiv.appendChild(LoadingComponent(3));
+
+    return skeletonDiv;
+}
--- public/components/ProductMain.js
+++ public/components/ProductMain.js
@@ -1,14 +1,27 @@
 import MainProductCard from "./MainProductCard.js";
 import ProductCardRecommended from "./ProductCardRecommended.js";
+import MainProductSkeleton from "./MainProductSkeleton.js";
+import ProductCardRecommendedSkeleton from "./ProductCardRecommendedSkeleton.js";
+
 
 export default async (id) => {
     const main = document.createElement("main");
     main.classList.add("product-main")
-    
-    const product = await getProduct(id)
-  
-    main.appendChild(await MainProductCard(product));
-    
+    const mainProductDiv = document.createElement("div")
+    mainProductDiv.classList.add("main-product-div")
+    main.appendChild(mainProductDiv)
+    const mainProductSkeleton = MainProductSkeleton()
+    mainProductDiv.appendChild(mainProductSkeleton)
+
+    setTimeout(async () => {
+        const product = await getProduct(id)
+        const mainProductCard = await MainProductCard(product)
+        mainProductSkeleton.remove()
+        mainProductDiv.appendChild(mainProductCard)
+        await generateRecommended(recommendedProductsDiv)
+    }, 0);
+
+
     const recomendedH2 = document.createElement("h2")
     recomendedH2.innerText = "Quem viu esse, também levou esses :)"
     recomendedH2.classList.add("recommended-h2")
@@ -17,12 +30,15 @@ export default async (id) => {
     const recommendedProductsDiv = document.createElement("div")
     recommendedProductsDiv.classList.add("product-card-recommended-div")
     main.appendChild(recommendedProductsDiv)
-    await generateRecommended(recommendedProductsDiv)
+
+    for (let i = 0; i < 5; i++) {
+        recommendedProductsDiv.appendChild(ProductCardRecommendedSkeleton(`skeleton-${i}`))
+    }
 
     const descriptionDiv = document.createElement("div")
     descriptionDiv.classList.add("product-main-description")
     main.appendChild(descriptionDiv)
-    
+
     const descriptionH1 = document.createElement("h2")
     descriptionH1.innerText = "Descrição do produto"
     descriptionDiv.appendChild(descriptionH1)
@@ -63,7 +79,7 @@ const getProduct = async (id) => {
     return data;
 }
 
-const getRecommended= async (min, max) => {
+const getRecommended = async (min, max) => {
     try {
         const response = await fetch(`/api/product/interval?min=${min}&max=${max}`, {
             method: "GET",
@@ -78,13 +94,17 @@ const getRecommended= async (min, max) => {
     }
 }
 
-const generateRecommended = async (element) => {
+const generateRecommended = async (append) => {
     const products = await getRecommended(1, 5);
     const productCards = await Promise.all(products.map(async (product) => {
         return await ProductCardRecommended(product.id);
     }));
 
+    for (let i = 0; i < 5; i++) {
+        document.getElementById(`skeleton-${i}`).remove()
+    }
+
     productCards.forEach((card) => {
-        element.appendChild(card);
+        append.appendChild(card);
     });
 }
